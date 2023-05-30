827268c2fea0eeafa90671489adbf147e4ce2ea6
Fakhri Yusuf Alfiansyah <fakhriyalfiansyah@gmail.com>
2023-05-30 10:30:40 +0700

    updated

--- app.py
+++ app.py
@@ -66,38 +66,3 @@ if __name__ == '__main__':
     app.run(debug=True,
             host="0.0.0.0",
             port=int(os.environ.get("PORT", 5000)))
-
-'''
-The ALLOWED_EXTENSIONS set is defined with the allowed file extensions as its elements. In this case, the allowed extensions are 'jpg', 'jpeg', and 'png'.
-The allowed_file function takes a filename as input.
-The function uses the rsplit method to split the filename into two parts based on the rightmost occurrence of the '.' character. This will separate the filename and the file extension.
-The second part of the split result, obtained with [1], represents the file extension.
-The function checks if the file extension obtained from step 4 is in lowercase and matches any of the allowed extensions defined in the ALLOWED_EXTENSIONS set.
-If the file extension is in lowercase and matches one of the allowed extensions, the function returns True, indicating that the file is allowed. Otherwise, it returns False.
-
-rsplit('.', 1) is used to split the filename into two parts based on the rightmost occurrence of the '.' character.
-
-The second parameter, 1, in rsplit('.', 1) specifies the maximum number of splits to perform. In this case, setting it to 1 means that the string will be split into two parts at most.
-Here's how it works:
-The rsplit method starts searching for the '.' character from the right side of the string (hence the 'r' in rsplit).
-When it finds the rightmost '.' character, it splits the string into two parts. The part before the '.' character becomes the first part, and the part after the '.' character becomes the second part.
-By setting the second parameter to 1, it ensures that only one split is performed. This is useful when dealing with filenames because it allows you to extract the file extension while preserving the rest of the filename intact.
-For example, if the filename is "image.jpg", filename.rsplit('.', 1) will return a list with two elements: ['image', 'jpg']. The first element is the filename without the extension, and the second element is the file extension.
-Overall, rsplit('.', 1) splits the filename into two parts at the rightmost occurrence of the '.' character, allowing you to extract the file extension.
-
-'''
-
-'''
-First, an instance of the Flask application is created with app = Flask(__name__). This initializes the Flask application.
-The UPLOAD_FOLDER variable is set as a configuration option in the Flask app using app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER. This specifies the folder where the uploaded files will be stored.
-The @app.route('/upload', methods=['POST']) decorator defines a route for the '/upload' URL with the HTTP method 'POST'. This means that this route will be triggered when a POST request is made to the '/upload' URL.
-The upload_file() function is the handler for the '/upload' route. It is executed when a POST request is made to the '/upload' URL.
-Inside the upload_file() function, it checks if the 'file' key is present in the request files using 'file' not in request.files. If it is not present, it returns a JSON response with an error message and a status code of 400 (Bad Request).
-If the 'file' key is present, it retrieves the uploaded file from the request using file = request.files['file']. The 'file' here corresponds to the name of the file input field in the HTML form that submits the file.
-It checks if the filename is empty with file.filename == ''. If it is empty, it returns a JSON response with an error message and a status code of 400.
-If the filename is not empty and the file has an allowed file extension (checked using the allowed_file() function), it proceeds to save the file.
-The secure_filename() function is used to ensure that the filename is secure and does not contain any potentially dangerous characters.
-The file is saved to the specified upload folder by joining the UPLOAD_FOLDER path with the secure filename using os.path.join(app.config['UPLOAD_FOLDER'], filename).
-Finally, it returns a JSON response with a success message and any additional information you want to include, such as a prediction result.
-Overall, this code handles file uploads by checking for the presence of the file, validating the filename and file extension, and saving the file to the specified upload folder. It provides appropriate error messages in case of missing or invalid files and returns a success message upon successful file upload.
-'''
